085368432326b5ede3860faf25538f9737fd0a50
D1n0s <gigog456@gmail.com>
2023-09-26 16:50:10 +0200

    wyglad mytrips

--- resources/views/myTrips.blade.php
+++ resources/views/myTrips.blade.php
@@ -1,42 +1,61 @@
 @extends('layouts.app')
-    @section('styles')
-        <link href="{{ asset('css/buttons.css') }}" rel="stylesheet">
-    @endsection
+@section('styles')
+    <link href="{{ asset('css/buttons.css') }}" rel="stylesheet">
+@endsection
 @section('content')
-    <style>             .pagination {
-            display: flex;
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap");
+
+        .pagination {
+            padding: 2vh;
+            border-radius: 8px;
+            background-color: rgba(21, 33, 72, 0.85);
+            width: 50%;
             justify-content: center; /* Wyśrodkowanie w poziomie */
             align-items: center; /* Wyśrodkowanie w pionie */
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+
         }
 
 
         .pagination a,
         .pagination .current {
+            font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
             display: inline-block;
             padding: 5px 10px;
             margin-right: 5px;
-            background-color: #007bff;
+            background-color: #000000;
             color: #fff;
-            text-decoration: none;
-            border-radius: 3px;
+            text-decoration: #0cde17;
+            border-radius: 5px;
+            background: $white;
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
         }
 
         .pagination a:hover {
             background-color: #0056b3;
         }
-
+        .center{
+            display: flex;
+            justify-content: center; /* Wyśrodkowanie w poziomie */
+            align-items: center; /* Wyśrodkowanie w pionie */
+        }
         /*MOTION FOR ROW*/
         .pow {
-    background-color: white;
+            background-image: linear-gradient(135deg, rgba(74, 205, 222, 0.25), rgba(29, 123, 219, 0.2) 20%, #152148 40%, rgba(21, 33, 72, 0.8) 100%);
             padding-top:5vh;
             padding-bottom: 5vh;
             min-height: 80vh;
             max-height: 120vh;
+            color:white;
+            padding: 1vh;
+
+
         }
 
         .framed {
-           {{--border-image-source: url({{asset('storage/path31.png')}});--}}
-            /*border-image-source: url("https://raw.githubusercontent.com/robole/artifice/main/framed-content/img/frame.png");*/
+        {{--border-image-source: url({{asset('storage/path31.png')}});--}}
+/*border-image-source: url("https://raw.githubusercontent.com/robole/artifice/main/framed-content/img/frame.png");*/
             border-image:url({{asset('storage/path31.png')}}) 1;
             border-image-slice: 85 140 140 310;
             border-image-repeat: stretch;
@@ -57,79 +76,86 @@
 
         }
 
-        #frame{
-
-        }
     </style>
+    @if(!empty($trips))
+        <div class=" framed container_content " style="padding: 0;">
+            <div class="text-center pow"  >
 
-    <div class=" framed container_content" style="padding: 0;">
-        <div class="text-center pow"  >
-
-            <!-- resources/views/mytrips.blade.php -->
-            <h1>Moje podróże</h1>
-<br> <br>
-            <table class="table table-bordered table-hover text-center">
-                <thead>
-                <tr>
-                    <th>Tytuł</th>
-                    <th>Opis</th>
-                    <th>Data wyjazdu</th>
-                    <th>Data przyjazdu</th>
-                    <th>AKCJE</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($trips as $trip)
-<form action="map/{{ $trip->trip_id }}?" method="get">
-                    <tr >
-                        <td>{{ $trip->title }}</td>
-                        <td>{{ $trip->desc }}</td>
-                        <td>{{ $trip->start_date }}</td>
-                        <td>{{ $trip->end_date }}</td>
-                        <td class="panel green"><button type="submit" >Wejdź</button></td>
-                        <td class="panel pink"><button>Usuń</button></td>
-
+                <!-- resources/views/mytrips.blade.php -->
+                <h1>Moje podróże</h1>
+                <br> <br><BR>
+                <table class="table  text-center text-white" style="vertical-align: middle;">
+                    <thead>
+                    <tr>
+                        <th>Tytuł</th>
+                        <th>Data wyjazdu</th>
+                        <th>Data przyjazdu</th>
+                        <th>AKCJE</th>
                     </tr>
-</form>
-                @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    @foreach ($trips as $trip)
+                        <form action="map/{{ $trip->trip_id }}?" method="get">
+                            <tr >
+                                <td>{{ $trip->title }}</td>
 
-            <!-- Paginacja -->
-            <div class="pagination">
-                @if ($trips->currentPage() > 1)
-                    <a href="{{ $trips->previousPageUrl() }}">Previous</a>
-                @endif
+                                <td>{{ $trip->start_date }}</td>
+                                <td>{{ $trip->end_date }}</td>
+                                <td class="panel green"><button type="submit" style="margin-right: 5vh;" >Wejdź</button>
+                                    <span class="panel pink">  <button>Usuń</button> </span>
+                                </td>
 
-                @if ($trips->currentPage() > 3)
-                    <a href="{{ $trips->url(1) }}">1</a>
-                    @if ($trips->currentPage() > 4)
-                        <span>...</span>
-                    @endif
-                @endif
+                            </tr>
+                        </form>
+                    @endforeach
+                    </tbody>
+                </table>
 
-                @for ($i = max(1, $trips->currentPage() - 2); $i <= min($trips->currentPage() + 2, $trips->lastPage()); $i++)
-                    @if ($i == $trips->currentPage())
-                        <span class="current">{{ $i }}</span>
-                    @else
-                        <a href="{{ $trips->url($i) }}">{{ $i }}</a>
-                    @endif
-                @endfor
+                <!-- Paginacja -->
+                <div class="center">
+                    <div class="pagination">
+                        @if ($trips->currentPage() > 1)
+                            <a href="{{ $trips->previousPageUrl() }}">POPRZEDNIE</a>
+                        @endif
 
-                @if ($trips->currentPage() < $trips->lastPage() - 2)
-                    @if ($trips->currentPage() < $trips->lastPage() - 3)
-                        <span>...</span>
-                    @endif
-                    <a href="{{ $trips->url($trips->lastPage()) }}">{{ $trips->lastPage() }}</a>
-                @endif
+                        @if ($trips->currentPage() > 3)
+                            <a href="{{ $trips->url(1) }}">1</a>
+                            @if ($trips->currentPage() > 4)
+                                <span>...</span>
+                            @endif
+                        @endif
 
-                @if ($trips->currentPage() < $trips->lastPage())
-                    <a href="{{ $trips->nextPageUrl() }}">Next</a>
-                @endif
+                        @for ($i = max(1, $trips->currentPage() - 2); $i <= min($trips->currentPage() + 2, $trips->lastPage()); $i++)
+                            @if ($i == $trips->currentPage())
+                                <span class="current">{{ $i }}</span>
+                            @else
+                                <a href="{{ $trips->url($i) }}">{{ $i }}</a>
+                            @endif
+                        @endfor
+
+                        @if ($trips->currentPage() < $trips->lastPage() - 2)
+                            @if ($trips->currentPage() < $trips->lastPage() - 3)
+                                <span>...</span>
+                            @endif
+                            <a href="{{ $trips->url($trips->lastPage()) }}">{{ $trips->lastPage() }}</a>
+                        @endif
+
+                        @if ($trips->currentPage() < $trips->lastPage())
+                            <a href="{{ $trips->nextPageUrl() }}">DALEJ</a>
+                        @endif
+                    </div>
+                </div>
             </div>
+
         </div>
 
-    </div>
+    @else
+        <h1>WITTTOOOOOOM</h1>
+    @endif
+
+
+
+
 
 
 
--- storage/framework/views/a799202a33853e34863e9754c49e7a0a.php
+++ storage/framework/views/a799202a33853e34863e9754c49e7a0a.php
@@ -1,41 +1,60 @@
-    <?php $__env->startSection('styles'); ?>
-        <link href="<?php echo e(asset('css/buttons.css')); ?>" rel="stylesheet">
-    <?php $__env->stopSection(); ?>
+<?php $__env->startSection('styles'); ?>
+    <link href="<?php echo e(asset('css/buttons.css')); ?>" rel="stylesheet">
+<?php $__env->stopSection(); ?>
 <?php $__env->startSection('content'); ?>
-    <style>             .pagination {
-            display: flex;
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap");
+
+        .pagination {
+            padding: 2vh;
+            border-radius: 8px;
+            background-color: rgba(21, 33, 72, 0.85);
+            width: 50%;
             justify-content: center; /* Wyśrodkowanie w poziomie */
             align-items: center; /* Wyśrodkowanie w pionie */
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+
         }
 
 
         .pagination a,
         .pagination .current {
+            font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
             display: inline-block;
             padding: 5px 10px;
             margin-right: 5px;
-            background-color: #007bff;
+            background-color: #000000;
             color: #fff;
-            text-decoration: none;
-            border-radius: 3px;
+            text-decoration: #0cde17;
+            border-radius: 5px;
+            background: $white;
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
         }
 
         .pagination a:hover {
             background-color: #0056b3;
         }
-
+        .center{
+            display: flex;
+            justify-content: center; /* Wyśrodkowanie w poziomie */
+            align-items: center; /* Wyśrodkowanie w pionie */
+        }
         /*MOTION FOR ROW*/
         .pow {
-    background-color: white;
+            background-image: linear-gradient(135deg, rgba(74, 205, 222, 0.25), rgba(29, 123, 219, 0.2) 20%, #152148 40%, rgba(21, 33, 72, 0.8) 100%);
             padding-top:5vh;
             padding-bottom: 5vh;
             min-height: 80vh;
             max-height: 120vh;
+            color:white;
+            padding: 1vh;
+
+
         }
 
         .framed {
-           
-            /*border-image-source: url("https://raw.githubusercontent.com/robole/artifice/main/framed-content/img/frame.png");*/
+        
+/*border-image-source: url("https://raw.githubusercontent.com/robole/artifice/main/framed-content/img/frame.png");*/
             border-image:url(<?php echo e(asset('storage/path31.png')); ?>) 1;
             border-image-slice: 85 140 140 310;
             border-image-repeat: stretch;
@@ -56,79 +75,86 @@
 
         }
 
-        #frame{
-
-        }
     </style>
+    <?php if(!empty($trips)): ?>
+        <div class=" framed container_content " style="padding: 0;">
+            <div class="text-center pow"  >
 
-    <div class=" framed container_content" style="padding: 0;">
-        <div class="text-center pow"  >
-
-            <!-- resources/views/mytrips.blade.php -->
-            <h1>Moje podróże</h1>
-<br> <br>
-            <table class="table table-bordered table-hover text-center">
-                <thead>
-                <tr>
-                    <th>Tytuł</th>
-                    <th>Opis</th>
-                    <th>Data wyjazdu</th>
-                    <th>Data przyjazdu</th>
-                    <th>AKCJE</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php $__currentLoopData = $trips; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $trip): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-<form action="map/<?php echo e($trip->trip_id); ?>?" method="get">
-                    <tr >
-                        <td><?php echo e($trip->title); ?></td>
-                        <td><?php echo e($trip->desc); ?></td>
-                        <td><?php echo e($trip->start_date); ?></td>
-                        <td><?php echo e($trip->end_date); ?></td>
-                        <td class="panel green"><button type="submit" >Wejdź</button></td>
-                        <td class="panel pink"><button>Usuń</button></td>
-
+                <!-- resources/views/mytrips.blade.php -->
+                <h1>Moje podróże</h1>
+                <br> <br><BR>
+                <table class="table  text-center text-white" style="vertical-align: middle;">
+                    <thead>
+                    <tr>
+                        <th>Tytuł</th>
+                        <th>Data wyjazdu</th>
+                        <th>Data przyjazdu</th>
+                        <th>AKCJE</th>
                     </tr>
-</form>
-                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    <?php $__currentLoopData = $trips; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $trip): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <form action="map/<?php echo e($trip->trip_id); ?>?" method="get">
+                            <tr >
+                                <td><?php echo e($trip->title); ?></td>
 
-            <!-- Paginacja -->
-            <div class="pagination">
-                <?php if($trips->currentPage() > 1): ?>
-                    <a href="<?php echo e($trips->previousPageUrl()); ?>">Previous</a>
-                <?php endif; ?>
+                                <td><?php echo e($trip->start_date); ?></td>
+                                <td><?php echo e($trip->end_date); ?></td>
+                                <td class="panel green"><button type="submit" style="margin-right: 5vh;" >Wejdź</button>
+                                    <span class="panel pink">  <button>Usuń</button> </span>
+                                </td>
 
-                <?php if($trips->currentPage() > 3): ?>
-                    <a href="<?php echo e($trips->url(1)); ?>">1</a>
-                    <?php if($trips->currentPage() > 4): ?>
-                        <span>...</span>
-                    <?php endif; ?>
-                <?php endif; ?>
+                            </tr>
+                        </form>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+                </table>
 
-                <?php for($i = max(1, $trips->currentPage() - 2); $i <= min($trips->currentPage() + 2, $trips->lastPage()); $i++): ?>
-                    <?php if($i == $trips->currentPage()): ?>
-                        <span class="current"><?php echo e($i); ?></span>
-                    <?php else: ?>
-                        <a href="<?php echo e($trips->url($i)); ?>"><?php echo e($i); ?></a>
-                    <?php endif; ?>
-                <?php endfor; ?>
+                <!-- Paginacja -->
+                <div class="center">
+                    <div class="pagination">
+                        <?php if($trips->currentPage() > 1): ?>
+                            <a href="<?php echo e($trips->previousPageUrl()); ?>">POPRZEDNIE</a>
+                        <?php endif; ?>
 
-                <?php if($trips->currentPage() < $trips->lastPage() - 2): ?>
-                    <?php if($trips->currentPage() < $trips->lastPage() - 3): ?>
-                        <span>...</span>
-                    <?php endif; ?>
-                    <a href="<?php echo e($trips->url($trips->lastPage())); ?>"><?php echo e($trips->lastPage()); ?></a>
-                <?php endif; ?>
+                        <?php if($trips->currentPage() > 3): ?>
+                            <a href="<?php echo e($trips->url(1)); ?>">1</a>
+                            <?php if($trips->currentPage() > 4): ?>
+                                <span>...</span>
+                            <?php endif; ?>
+                        <?php endif; ?>
 
-                <?php if($trips->currentPage() < $trips->lastPage()): ?>
-                    <a href="<?php echo e($trips->nextPageUrl()); ?>">Next</a>
-                <?php endif; ?>
+                        <?php for($i = max(1, $trips->currentPage() - 2); $i <= min($trips->currentPage() + 2, $trips->lastPage()); $i++): ?>
+                            <?php if($i == $trips->currentPage()): ?>
+                                <span class="current"><?php echo e($i); ?></span>
+                            <?php else: ?>
+                                <a href="<?php echo e($trips->url($i)); ?>"><?php echo e($i); ?></a>
+                            <?php endif; ?>
+                        <?php endfor; ?>
+
+                        <?php if($trips->currentPage() < $trips->lastPage() - 2): ?>
+                            <?php if($trips->currentPage() < $trips->lastPage() - 3): ?>
+                                <span>...</span>
+                            <?php endif; ?>
+                            <a href="<?php echo e($trips->url($trips->lastPage())); ?>"><?php echo e($trips->lastPage()); ?></a>
+                        <?php endif; ?>
+
+                        <?php if($trips->currentPage() < $trips->lastPage()): ?>
+                            <a href="<?php echo e($trips->nextPageUrl()); ?>">DALEJ</a>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </div>
+
         </div>
 
-    </div>
+    <?php else: ?>
+        <h1>WITTTOOOOOOM</h1>
+    <?php endif; ?>
+
+
+
+
 
 
 
